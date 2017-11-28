dbcc34b7f3e1f5e47e36374b7faa3618ec081982
Johanna Schreiner <jschreinerdesign@gmail.com>
2017-11-28 16:34:35 +1100

    modified index5.js

--- Johanna Schreiner/wk01/2-tue/ex5/index5.js
+++ Johanna Schreiner/wk01/2-tue/ex5/index5.js
@@ -1,6 +1,6 @@
 var currentAge = prompt("How old are you?");
 var maximumAge = prompt("How long do you think you will live?");
 var estimatedAmountPerDay = prompt("How many do you think you'll eat per day?");
-var lifetimeSupply = (maximumAge - currentAge) * estimatedAmountPerDay;
+var lifetimeSupply = (maximumAge - currentAge) * (estimatedAmountPerDay * 365);
 
 console.log("You will need " + lifetimeSupply + " to last you until the ripe old age of " + maximumAge + ".");
