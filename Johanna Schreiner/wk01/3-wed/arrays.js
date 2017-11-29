915175d8867ca9f67e22b7ecfd309ce6cb16a414
Johanna Schreiner <jschreinerdesign@gmail.com>
2017-11-29 23:44:03 +1100

    Wednesday Arrays hw

---
+++ Johanna Schreiner/wk01/3-wed/arrays.js
@@ -0,0 +1,12 @@
+
+
+var daysOfTheWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+
+daysOfTheWeek.pop();
+
+daysOfTheWeek.unshift("Sunday");
+
+daysOfTheWeek.shift();
+daysOfTheWeek.pop();
+
+daysOfTheWeek.sort();
