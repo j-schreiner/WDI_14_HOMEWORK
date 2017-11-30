3c0ece48721f8be60556e3f305cad733c28b5ba8
Johanna Schreiner <jschreinerdesign@gmail.com>
2017-11-30 22:30:22 +1100

    thurs hw

---
+++ Johanna Schreiner/wk01/4-thu/pt_planner/pt_planner.js
@@ -0,0 +1,40 @@
+
+
+var alamein = ["Flinders St","Richmond","East Richmond","Burnley","Hawthorn","Glenferrie"]
+
+var glenWaverley = ["Flagstaff","Melbourne Central","Parliament","Richmond","Kooyong","Tooronga"]
+
+var sandringham = ["Southern Cross","Richmond","South Yarra","Prahran","Windsor"]
+
+
+// var origin = prompt ("Departing from?")
+// var destination = prompt ("Arriving at?")
+
+var origin = "Burnley"
+var destination = "Windsor"
+
+
+
+
+
+// var sortAlamein = alamein.sort()
+// var sortGlenWaverley = glenWaverley.sort()
+// var sortSandringham = sandringham.sort()
+
+
+// console.log(sortAlamein)
+// console.log(sortGlenWaverley)
+// console.log(sortSandringham)
+
+// var intersect = (alamein, glenWaverley, sandringham)
+
+// console.log(intersect)
+
+
+// var userOrigin = prompt ("Departing from?")
+// var userDestination = prompt ("Arriving at?")
+
+
+// var intersect = 
+
+// console.log(userOrigin + intersect + userDestination)
