1ea6a70e98cac2e8f56b1cbdb3913252810ba3eb
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-01-23 09:48:03 +1100

    finished stocks

--- Johanna Schreiner/wk04/5-fri/stocks/main.rb
+++ Johanna Schreiner/wk04/5-fri/stocks/main.rb
@@ -2,9 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "stock_quote"
 
-get '/data' do
-  chosen_stock = params[:stock]
-  stock_data = StockQuote::Stock.quote("#{chosen_stock}")
-  @stock = stock_data
+
+get '/' do
+  stock = StockQuote::Stock.json_quote(params["stock"])
+  @name = stock["name"]
+  @symbol = stock["symbol"]
+  @exchange = stock["exchange"]
+  @l = stock["l"]
+  @op = stock["op"]
+  @hi = stock["hi"]
+  @lo = stock["lo"]
+  @vo = stock["vo"]
+  @c = stock["c"]
+  @cp = stock["cp"]
   erb :index
 end
+
