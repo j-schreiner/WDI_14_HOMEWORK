d9d7ce049cb0fefe71cc6d5bd6701e4b77290d52
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-01-28 13:53:15 +1100

    dropdown

---
+++ Johanna Schreiner/wk07/4-thu/drop_down/main.js
@@ -0,0 +1,53 @@
+console.log("test")
+
+
+var cityType = document.querySelector("#city-type");
+
+var NYC = document.createElement("option");
+var SF = document.createElement("option");
+var LA = document.createElement("option");
+var SYD = document.createElement("option");
+var ATX = document.createElement("option");
+
+NYC.value = "NYC";
+NYC.text = "NYC";
+
+SF.value = "SF";
+SF.text = "SF";
+
+LA.value = "LA";
+LA.text = "LA";
+
+SYD.value = "SYD";
+SYD.text = "SYD";
+
+ATX.value = "ATX";
+ATX.text = "ATX";
+
+cityType.add(NYC, null);
+cityType.add(SF, null);
+cityType.add(LA, null);
+cityType.add(SYD, null);
+cityType.add(ATX, null);
+
+
+
+var backgroundImage = document.querySelector("body")
+
+function changeImage(){
+
+  if (cityType.value == "NYC"){
+    backgroundImage.classList.add('nyc')
+  } else if(cityType.value == "SF"){
+    backgroundImage.classList.add('sf')
+  } else if (cityType.value == "LA"){
+    backgroundImage.classList.add('la')
+  } else if (cityType.value == "SYD"){
+    backgroundImage.classList.add('sydney')
+  } else if (cityType.value == "ATX"){
+    backgroundImage.classList.add('austin')
+  } 
+ 
+}
+
+cityType.addEventListener("change", changeImage)
