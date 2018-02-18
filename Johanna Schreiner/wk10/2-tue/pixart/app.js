a5b85eaba79821e179cde0d6868599703b3035b8
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-02-18 15:11:48 +1100

    commit 7 / solutions

--- Johanna Schreiner/wk10/2-tue/pixart/app.js
+++ Johanna Schreiner/wk10/2-tue/pixart/app.js
@@ -2,9 +2,24 @@ $('.color-choice').keypress(function(event){
   var keycode = event.keyCode || event.which
 
   if(keycode == '13') {
+    event.preventDefault()
     $colorChoice = $('.color-choice').val()
     $('.brush').css("background-color", $colorChoice )
+    
+  }
+})
+
+
+$('.movie-choice').keypress(function(event){
+  var keycode = event.keyCode || event.which
+
+  if(keycode == '13') {
     event.preventDefault()
+    title = $('.movie-choice').val()
+    $.get('http://omdbapi.com?apikey=2f6435d9&t=' + title)
+    .done(function(movie) {
+      $('.brush').css('background-image', 'url(' + movie.Poster + ')')
+    })
   }
 })
 
@@ -12,9 +27,8 @@ $('.color-choice').keypress(function(event){
 for (var i = 0; i <= 100; i++){
   var $newSquareDiv = $('<div>', {"class": "square"})
   $('.squares').append($newSquareDiv)
+
 }
-
-
 
 $('.squares').on('mouseover', '.square', function(event){
   $(event.target).css("background-color", $colorChoice)
